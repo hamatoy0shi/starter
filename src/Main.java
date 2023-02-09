0d5b18df3cb6e7f4fc376fbb615f30d4b99a8ad2
Ivan <ivan@Ivans-MacBook-Air.local>
2023-02-09 16:39:20 +0300

    Рефакторинг кода

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+
+    static Pattern pattern = Pattern.compile("\\d+");
     public static void main(String[] args) {
         if (args.length == 0) System.exit(0);
-        System.out.println(findPattern(args[0], 0));
+        Matcher matcher = pattern.matcher(args[0]);
+        int result = 0;
+
+        while (matcher.find()) {
+           result += Integer.parseInt(matcher.group());
+        }
+
+        System.out.println(result);
     }
 
-    public static Integer findPattern(String text, Integer currentRes) {
-        Integer nextFound = 0;
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(text);
-        boolean found = matcher.find();
-        if (found) {
-            String matchFound = matcher.group();
-            nextFound = currentRes + Integer.parseInt(matchFound);
-            currentRes = findPattern(matcher.replaceFirst(""), nextFound);
-        }
-        return (currentRes);
-    }
 }
